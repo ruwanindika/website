d9d9ab8f049b4a1e16a1e36efc6afca77b9d4943
ruwanindika <ruwanindika@gmail.com>
2024-08-30 14:00:51 +1000

    test

--- cdk-infra/lib/cdk-infra-stack.ts
+++ cdk-infra/lib/cdk-infra-stack.ts
@@ -3,19 +3,13 @@ import { Construct } from 'constructs';
 import {Bucket, BucketAccessControl} from "aws-cdk-lib/aws-s3";
 import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
 import * as path from "path";
+import {Distribution, OriginAccessIdentity} from "aws-cdk-lib/aws-cloudfront";
+import {S3Origin} from "aws-cdk-lib/aws-cloudfront-origins";
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
 
     const bucket = new Bucket(this, 'Bucket', {
       accessControl: BucketAccessControl.PRIVATE,
@@ -26,5 +20,15 @@ export class CdkInfraStack extends cdk.Stack {
       sources: [Source.asset(path.resolve(__dirname, '../../dist'))]
     })
     
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+    bucket.grantRead(originAccessIdentity);
+    
+    new Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(bucket, {originAccessIdentity}),
+      },
+    })
+
   }
 }
