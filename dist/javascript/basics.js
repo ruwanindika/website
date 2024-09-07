9cf3fa4902d3f9e828bdbf572470ce7c2834ab7c
ruwanindika <ruwanindika@gmail.com>
2024-09-07 17:45:29 +1000

    added an image galery

---
+++ dist/javascript/basics.js
@@ -0,0 +1,37 @@
+// const myButton = document.getElementById("myButton");
+// myButton.addEventListener("click", function() {
+//   // add your JavaScript code here
+//   let d = new Date();
+//     alert("Today's date is " + d);
+// });
+
+
+var index = 1
+
+var firstTime = true
+
+function myFunction() {
+
+    if (firstTime) {
+        document.getElementById("centerImg").src = './images/pages/Sinhala-for-kids-1.jpg';
+
+    } else {
+    index = getRandomInt(27)
+    document.getElementById("centerImg").src = './images/pages/Sinhala-for-kids-'+index+'.jpg';
+    }
+
+
+    firstTime = false
+    
+}
+
+function getRandomInt(max) {
+    var myNum = Math.floor(Math.random() * max);
+
+    if (myNum==0)
+    {myNum = 1}
+
+    return myNum
+  }
+
+
