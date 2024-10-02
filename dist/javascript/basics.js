cca993f3f79226da7b05ad06fe5163acb8e3d6e1
ruwanindika <ruwanindika@gmail.com>
2024-10-02 10:33:53 +1000

    Fixed the header margins

--- dist/javascript/basics.js
+++ dist/javascript/basics.js
@@ -5,33 +5,29 @@
 //     alert("Today's date is " + d);
 // });
 
+var index = 1;
 
-var index = 1
-
-var firstTime = true
+var firstTime = true;
 
 function myFunction() {
+  if (firstTime) {
+    document.getElementById("book-img").src =
+      "./images/pages/Sinhala-for-kids-1.jpg";
+  } else {
+    index = getRandomInt(27);
+    document.getElementById("book-img").src =
+      "./images/pages/Sinhala-for-kids-" + index + ".jpg";
+  }
 
-    if (firstTime) {
-        document.getElementById("book-img").src = './images/pages/Sinhala-for-kids-1.jpg';
-
-    } else {
-    index = getRandomInt(27)
-    document.getElementById("book-img").src = './images/pages/Sinhala-for-kids-'+index+'.jpg';
-    }
-
-
-    firstTime = false
-    
+  firstTime = false;
 }
 
 function getRandomInt(max) {
-    var myNum = Math.floor(Math.random() * max);
+  var myNum = Math.floor(Math.random() * max);
 
-    if (myNum==0)
-    {myNum = 1}
-
-    return myNum
+  if (myNum == 0) {
+    myNum = 1;
   }
 
-
+  return myNum;
+}
