1217dd5b6f74f366ed88b18250e021a4cf1c8042
ruwanindika <ruwanindika@gmail.com>
2024-10-06 10:54:25 +1100

    update aws regions

--- cdk-infra/lib/defaults.ts
+++ cdk-infra/lib/defaults.ts
@@ -1,5 +1,5 @@
 export const PROD_URL = "sinhalaforkids.com";
 export const BETA_URL = "beta.sinhalaforkids.com";
 
-export const PROD_ENV = { account: "161580273020", region: "eu-east-1" };
-export const BETA_ENV = { account: "161580273020", region: "eu-west-2" };
+export const PROD_ENV = { account: "161580273020", region: "us-east-1" };
+export const BETA_ENV = { account: "161580273020", region: "ap-southeast-2" };
