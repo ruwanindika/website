6742e3fa7ac9de306d1f576698ff56b3e8e2b63b
Ruwan Indika <ruwanindika@gmail.com>
2024-10-22 03:51:52 +0000

    Added cdk testcases [ci skip]

--- cdk-infra/test/cdk-infra.test.ts
+++ cdk-infra/test/cdk-infra.test.ts
@@ -1,16 +1,21 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkInfra from '../lib/cdk-infra-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as CdkInfra from "../lib/cdk-infra-stack";
+import * as defaults from "../lib/defaults";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-infra-stack.ts
-test("SQS Queue Created", () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new CdkInfra.CdkInfraStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+
+const app = new cdk.App();
+const stack = new CdkInfra.CdkInfraStack(app, "MyTestStack", {
+  env: defaults.PROD_ENV,
+  deploymentStage: "prod",
+});
+
+const template = Template.fromStack(stack);
+
+test("Web site s3 bucket is private", () => {
+  template.hasResourceProperties("AWS::S3::Bucket", {
+    AccessControl: "Private",
+  });
 });
