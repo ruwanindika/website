8adea7bf52f1c99ccdd5019192ff99c53c9bec90
Ruwan Indika <ruwanindika@gmail.com>
2024-10-22 05:11:24 +0000

    added cdk testing

--- cdk-infra/test/cdk-infra.test.ts
+++ cdk-infra/test/cdk-infra.test.ts
@@ -6,16 +6,23 @@ import * as defaults from "../lib/defaults";
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-infra-stack.ts
 
+// PROD stage testing
 const app = new cdk.App();
-const stack = new CdkInfra.CdkInfraStack(app, "MyTestStack", {
+const stack = new CdkInfra.CdkInfraStack(app, "MyTestStack-PROD", {
   env: defaults.PROD_ENV,
   deploymentStage: "prod",
 });
 
 const template = Template.fromStack(stack);
 
+
+
+  
 test("Web site s3 bucket is private", () => {
   template.hasResourceProperties("AWS::S3::Bucket", {
     AccessControl: "Private",
   });
 });
+
+  
+
